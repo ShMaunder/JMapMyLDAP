8314be319699c1b72578e9a3857f1dbd27154e62
Shaun Maunder <shmaunder@yahoo.co.uk>
2013-04-06 16:45:38 +0100

    Added missing custom exceptions

---
+++ libraries/shmanic/exception/invaliduser.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * PHP Version 5.3
+ *
+ * @package     Shmanic.Libraries
+ * @subpackage  Exception
+ * @author      Shaun Maunder <shaun@shmanic.com>
+ *
+ * @copyright   Copyright (C) 2011-2013 Shaun Maunder. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Exception that also stores a username when a invalid user error occurs.
+ *
+ * @package     Shmanic.Libraries
+ * @subpackage  Ldap
+ * @since       2.0
+ */
+class SHExceptionInvaliduser extends Exception
+{
+	/**
+	 * Affected username.
+	 *
+	 * @var    string
+	 * @since  2.0
+	 */
+	protected $username = null;
+
+	/**
+	 * Constructor
+	 *
+	 * @param   string   $message   Error message.
+	 * @param   integer  $code      Error ID.
+	 * @param   integer  $username  Username of user.
+	 *
+	 * @since   2.0
+	 */
+	public function __construct($message = null, $code = 0, $username = null)
+	{
+		$this->username = $username;
+
+		parent::__construct($message, $code, null);
+	}
+
+	/**
+	 * Magic method to override the to string value for the exception.
+	 *
+	 * @return  string  Exception string.
+	 *
+	 * @since   2.0
+	 */
+	public function __toString()
+	{
+		// Print with username string attached
+		return sprintf(
+			'Exception %1$d message \'%2$s\' for user \'%3$s\' in %3$s:%4$d',
+			$this->getCode(), $this->getMessage(), $this->getUsername(),
+			$this->getFile(), $this->getLine()
+		);
+	}
+
+	/**
+	 * Returns the affected username.
+	 *
+	 * @return  string
+	 *
+	 * @since   2.0
+	 */
+	public function getUsername()
+	{
+		return $this->username;
+	}
+}
---
+++ libraries/shmanic/exception/stacked.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * PHP Version 5.3
+ *
+ * @package     Shmanic.Libraries
+ * @subpackage  Exception
+ * @author      Shaun Maunder <shaun@shmanic.com>
+ *
+ * @copyright   Copyright (C) 2011-2013 Shaun Maunder. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Exception that stacks other exceptions in the same exception.
+ *
+ * @package     Shmanic.Libraries
+ * @subpackage  Ldap
+ * @since       2.0
+ */
+class SHExceptionStacked extends Exception
+{
+	/**
+	 * Stacked exceptions.
+	 *
+	 * @var    array
+	 * @since  2.0
+	 */
+	protected $stacked = array();
+
+	/**
+	 * Constructor
+	 *
+	 * @param   string   $message  Error message.
+	 * @param   integer  $code     Error ID.
+	 * @param   array    $stacked  An array of exceptions.
+	 *
+	 * @since   2.0
+	 */
+	public function __construct($message = null, $code = 0, $stacked = array())
+	{
+		$this->stacked = $stacked;
+
+		parent::__construct($message, $code, null);
+	}
+
+	/**
+	 * Magic method to override the to string value for the exception.
+	 *
+	 * @return  string  Exception string.
+	 *
+	 * @since   2.0
+	 */
+	public function __toString()
+	{
+		// TODO: need to add the stacked exceptions here somehow
+		return sprintf(
+			'Exception %1$d message \'%2$s\' in %3$s:%4$d',
+			$this->getCode(), $this->getMessage(),
+			$this->getFile(), $this->getLine()
+		);
+	}
+
+	/**
+	 * Gets the stacked exceptions array.
+	 *
+	 * @return  array
+	 *
+	 * @since   2.0
+	 */
+	public function getStacked()
+	{
+		return $this->stacked;
+	}
+}
