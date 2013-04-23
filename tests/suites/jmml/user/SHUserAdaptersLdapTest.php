28d7e378d7098b1de63ca18cf1def7bdf6977c93
Shaun Maunder <shmaunder@yahoo.co.uk>
2013-04-23 20:23:46 +0100

    Added initial basic unit tests for creation and password resets

--- tests/suites/jmml/user/SHUserAdaptersLdapTest.php
+++ tests/suites/jmml/user/SHUserAdaptersLdapTest.php
@@ -254,4 +254,114 @@ class SHLUserAdaptersLdapTest extends PHPUnit_Framework_TestCase
 		$this->fail('Incorrect ordering of exceptions');
 	}
 
+	public function testGetAttributesInvalidPasswordException()
+	{
+		$this->setExpectedException('SHExceptionInvaliduser', 'LIB_SHLDAP_ERR_10303', 10303);
+
+		$ldap = TestsHelper::getLdapConfig(216);
+		$user = TestsHelper::getUserCreds();
+		$user['password'] .= '!"£%^&*()(&^$£"';
+
+		$adapter = new SHUserAdaptersLdap($user, $ldap);
+
+		try
+		{
+			$adapter->getId(true);
+		}
+		catch (Exception $e)
+		{
+			$adapter->getAttributes();
+		}
+	}
+
+	public function testGetAttributesInvalidUserException()
+	{
+		$this->setExpectedException('SHExceptionInvaliduser', 'LIB_SHLDAP_ERR_10302', 10302);
+
+		$ldap = TestsHelper::getLdapConfig(216);
+		$user = TestsHelper::getUserCreds();
+		$user['username'] .= '!"£%^&*()(&^$£"';
+
+		$adapter = new SHUserAdaptersLdap($user, $ldap);
+
+		$adapter->getAttributes();
+	}
+
+	public function testGetAttributesDoGetId()
+	{
+		$ldap = TestsHelper::getLdapConfig(216);
+		$user = TestsHelper::getUserCreds();
+
+		$adapter = new SHUserAdaptersLdap($user, $ldap);
+
+		$attribute = $adapter->getAttributes($ldap['ldap_uid']);
+
+		$this->assertEquals($user[$ldap['ldap_uid']][0], $attribute[$ldap['ldap_uid']][0]);
+	}
+
+
+	public function testSetAttributes1()
+	{
+		$ldap = TestsHelper::getLdapConfig(216);
+
+		// Loop 50 times to test random users
+		for ($i = 0; $i < 50; $i++)
+		{
+			$user = TestsHelper::getUserCreds();
+
+			$adapter = new SHUserAdaptersLdap($user, $ldap);
+
+			// Save the current phone
+			$currentPhone = $adapter->getAttributes('telephoneNumber');
+
+			// Generate new phone number
+			$newPhone = rand(11111111, 99999999);
+
+			// Save the new phone number to the user
+			$changes = array('telephoneNumber' => array($newPhone));
+			$adapter->setAttributes($changes);
+			$this->assertTrue(JArrayHelper::getValue($adapter->commitChanges(), 'status'));
+			$this->assertTrue(JArrayHelper::getValue($adapter->commitChanges(), 'nochanges'));
+
+			// Test to see if the adapter has updated it own internal attributes
+			$this->assertEquals(array('telephoneNumber' => array($newPhone)), $adapter->getAttributes('telephoneNumber'));
+
+			// Test the new attribute
+			$testAdapter = new SHUserAdaptersLdap($user, $ldap);
+			$this->assertEquals(array('telephoneNumber' => array($newPhone)), $testAdapter->getAttributes('telephoneNumber'));
+
+			// Set back to the old attributes
+			$adapter->setAttributes($currentPhone);
+			$this->assertTrue(JArrayHelper::getValue($adapter->commitChanges(), 'status'));
+
+			// Test its changeed back
+			$testAdapter = new SHUserAdaptersLdap($user, $ldap);
+			$this->assertEquals($currentPhone, $testAdapter->getAttributes('telephoneNumber'));
+		}
+	}
+
+	public function testCreateDeleteUsers1()
+	{
+		$ldap = TestsHelper::getLdapConfig(216);
+
+		// Loop 50 times to test random users
+		for ($i = 0; $i < 50; $i++)
+		{
+			$user = TestsHelper::getUserCreds(null, 101);
+
+			// Create the new user
+			$adapter = new SHUserAdaptersLdap($user, $ldap, array('isNew' => 1));
+			$adapter->setAttributes($user);
+			$this->assertTrue($adapter->commitChanges());
+
+			// Test the new user
+			$testAdapter = new SHUserAdaptersLdap($user, $ldap);
+			$this->assertEquals($user['dn'], $testAdapter->getId(true));
+			$phone = JArrayHelper::getValue($testAdapter->getAttributes('telephoneNumber'), 'telephoneNumber');
+			$this->assertEquals($user['telephoneNumber'], $phone);
+
+			// Delete the new user
+			$this->assertTrue($adapter->delete());
+		}
+	}
 }
