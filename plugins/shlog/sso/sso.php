a1da81074164b5819a2e88a1218c534157196851
Shaun Maunder <shmaunder@yahoo.co.uk>
2013-05-30 01:44:12 +0100

    Added SSO SHLog logging plugin

---
+++ plugins/shlog/sso/sso.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * PHP Version 5.3
+ *
+ * @package     Shmanic.Plugin
+ * @subpackage  Log.Sso
+ * @author      Shaun Maunder <shaun@shmanic.com>
+ *
+ * @copyright   Copyright (C) 2011-2013 Shaun Maunder. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+jimport('joomla.plugin.plugin');
+
+/**
+ * SSO logging plugin.
+ *
+ * @package     Shmanic.Plugin
+ * @subpackage  Log.Sso
+ * @since       2.0
+ */
+class PlgShlogSso extends JPlugin
+{
+	const LOGGER_FILE = 'formattedtext';
+
+	const LOGGER_SCREEN = 'messagequeue';
+
+	const SSO_CATEGORY = 'sso';
+
+	const AUTH_CATEGORY = 'auth';
+
+	/**
+	 * Fired on log initialiser.
+	 *
+	 * @return  void
+	 *
+	 * @since   2.0
+	 */
+	public function onLogInitialise()
+	{
+		// This is the columns that the log files will use
+		$fileFormat = str_replace('\t', "\t", $this->params->get('FILE_FORMAT', '{DATETIME}\t{ID}\t{MESSAGE}'));
+
+		/*
+		 * Deals with the Information level logs.
+		 */
+		if ($this->params->get('enable_info', true))
+		{
+			// Setup a information file logger
+			JLog::addLogger(
+				array(
+					'logger' => self::LOGGER_FILE,
+					'text_file' => $this->params->get('log_name_info', 'sso.info.php'),
+					'text_entry_format' => $fileFormat
+				),
+				JLog::INFO,
+				array(self::SSO_CATEGORY, self::AUTH_CATEGORY)
+			);
+		}
+
+		/*
+		 * Deals with the Debugging level logs (which includes all levels internally).
+		 */
+		if ($this->params->get('enable_debug', true))
+		{
+			// Setup a debugger file logger
+			JLog::addLogger(
+				array(
+					'logger' => self::LOGGER_FILE,
+					'text_file' => $this->params->get('log_name_debug', 'sso.debug.php'),
+					'text_entry_format' => $fileFormat
+				),
+				JLog::ALL,
+				array(self::SSO_CATEGORY, self::AUTH_CATEGORY)
+			);
+		}
+
+		/*
+		 * Deals with the Error level logs.
+		 */
+		if ($this->params->get('enable_error', true))
+		{
+			// Setup a error file logger
+			JLog::addLogger(
+				array(
+					'logger' => self::LOGGER_FILE,
+					'text_file' => $this->params->get('log_name_error', 'sso.error.php'),
+					'text_entry_format' => $fileFormat
+				),
+				JLog::ERROR,
+				array(self::SSO_CATEGORY, self::AUTH_CATEGORY)
+			);
+
+			if ($this->params->get('error_to_screen', true))
+			{
+				// Setup a error on-screen logger
+				JLog::addLogger(
+					array('logger' => self::LOGGER_SCREEN),
+					JLog::ERROR,
+					array(self::SSO_CATEGORY, self::AUTH_CATEGORY)
+				);
+			}
+		}
+	}
+}
