b088b9c5eaa897ae3a93546175dc75dcb191404e
Shaun Maunder <shmaunder@yahoo.co.uk>
2013-05-30 02:02:54 +0100

    Added methods to set and get the auth type and domain from a user

--- libraries/shmanic/user/helper.php
+++ libraries/shmanic/user/helper.php
@@ -21,6 +21,10 @@ defined('JPATH_PLATFORM') or die;
  */
 abstract class SHUserHelper
 {
+	const PARAM_AUTH_TYPE = 'auth_type';
+
+	const PARAM_AUTH_DOMAIN = 'auth_domain';
+
 	/**
 	 * This method returns a user object. If options['autoregister'] is true,
 	 * and if the user doesn't exist, then it'll be created.
@@ -37,26 +41,63 @@ abstract class SHUserHelper
 	{
 		$instance = JUser::getInstance();
 
+		if (isset($options['adapter']))
+		{
+			// Tell the getUser to store the auth_type and auth_config based on whats inside the adapter
+			$options['type'] = isset($options['type']) ? $options['type'] : $options['adapter']::getType();
+			$options['domain'] = isset($options['domain']) ? $options['domain'] : $options['adapter']->getDomain();
+		}
+
 		// Check if the user already exists in the database
 		if ($id = intval(JUserHelper::getUserId($user['username'])))
 		{
 			$instance->load($id);
+
+			// Inject the type and domain into this object if they are set
+			if (isset($options['type']))
+			{
+				$instance->setParam(self::PARAM_AUTH_TYPE, $options['type']);
+			}
+
+			if (isset($options['domain']))
+			{
+				$instance->setParam(self::PARAM_AUTH_DOMAIN, $options['domain']);
+			}
+
 			return $instance;
 		}
 
-		jimport('joomla.application.component.helper');
-		$config	= JComponentHelper::getParams('com_users', true);
+		// ** The remainder of this method is for new users only **
 
-		if ($config === false)
+		$config = SHFactory::getConfig();
+
+		// Deal with auto registration flags
+		$autoRegister = (int) $config->get('user.autoregister', 1);
+
+		if ($autoRegister === 0 || $autoRegister === 1)
+		{
+			// Inherited Auto-registration
+			$options['autoregister'] = isset($options['autoregister']) ? $options['autoregister'] : $autoRegister;
+		}
+		else
+		{
+			// Override Auto-registration
+			$options['autoregister'] = ($autoRegister === 2) ? 1 : 0;
+		}
+
+		// Deal with the default group
+		jimport('joomla.application.component.helper');
+		$comUsers = JComponentHelper::getParams('com_users', true);
+
+		if ($comUsers === false)
 		{
 			// Check if there is a default set in the SHConfig
-			$config = SHFactory::getConfig();
 			$defaultUserGroup = $config->get('user.defaultgroup', 2);
 		}
 		else
 		{
 			// Respect Joomla's default user group
-			$defaultUserGroup = $config->get('new_usertype', 2);
+			$defaultUserGroup = $comUsers->get('new_usertype', 2);
 		}
 
 		// Setup the user fields for this new user
@@ -68,13 +109,19 @@ abstract class SHUserHelper
 		$instance->set('usertype', 'depreciated');
 		$instance->set('groups', array($defaultUserGroup));
 
-		// Set the authentication type as a parameter
-		$instance->setParam('authtype', strtoupper(JArrayHelper::getValue($options, 'type')));
+		// Set the User Adapter parameters
+		if (isset($options['type']))
+		{
+			$instance->setParam(self::PARAM_AUTH_TYPE, $options['type']);
+		}
+
+		if (isset($options['domain']))
+		{
+			$instance->setParam(self::PARAM_AUTH_DOMAIN, $options['domain']);
+		}
 
 		// If autoregister is set, register the user
-		$autoregister = isset($options['autoregister']) ? $options['autoregister'] : true;
-
-		if ($autoregister)
+		if ($options['autoregister'])
 		{
 			if (!self::save($instance))
 			{
@@ -96,14 +143,15 @@ abstract class SHUserHelper
 	 * the JUser::save() method to bypass ACL checks for super users. It still
 	 * calls the onUserBeforeSave and onUserAfterSave events.
 	 *
-	 * @param   JUser  &$user  Object to save.
+	 * @param   JUser    &$user     Object to save.
+	 * @param   Boolean  $dispatch  True to call the listening plugins or False to skip dispatching
 	 *
 	 * @return  boolean  True on success or False on failure.
 	 *
 	 * @since   2.0
 	 * @throws  Exception
 	 */
-	public static function save(JUser &$user)
+	public static function save(JUser &$user, $dispatch = true)
 	{
 		// Create the user table object
 		$table = $user->getTable();
@@ -120,21 +168,25 @@ abstract class SHUserHelper
 
 		$my = JFactory::getUser();
 
-		// Check if we are creating a new user
-		$isNew = empty($user->id);
-
-		// Get the old user
-		$oldUser = new JUser($user->id);
-
-		// Fire the onUserBeforeSave event.
-		JPluginHelper::importPlugin('user');
-		$dispatcher = JDispatcher::getInstance();
-
-		$result = $dispatcher->trigger('onUserBeforeSave', array($oldUser->getProperties(), $isNew, $user->getProperties()));
-		if (in_array(false, $result, true))
+		if ($dispatch)
 		{
-			// Plugin will have to raise its own error or throw an exception.
-			return false;
+			// Check if we are creating a new user
+			$isNew = empty($user->id);
+
+			// Get the old user
+			$oldUser = new JUser($user->id);
+
+			// Fire the onUserBeforeSave event.
+			JPluginHelper::importPlugin('user');
+			$dispatcher = JDispatcher::getInstance();
+
+			$result = $dispatcher->trigger('onUserBeforeSave', array($oldUser->getProperties(), $isNew, $user->getProperties()));
+
+			if (in_array(false, $result, true))
+			{
+				// Plugin will have to raise its own error or throw an exception.
+				return false;
+			}
 		}
 
 		// Store the user data in the database
@@ -156,10 +208,115 @@ abstract class SHUserHelper
 			$my->setParameters($registry);
 		}
 
-		// Fire the onUserAfterSave event
-		$dispatcher->trigger('onUserAfterSave', array($user->getProperties(), $isNew, $result, $user->getError()));
+		if ($dispatch)
+		{
+			// Fire the onUserAfterSave event
+			$dispatcher->trigger('onUserAfterSave', array($user->getProperties(), $isNew, $result, $user->getError()));
+		}
 
 		return $result;
+	}
+
+	/**
+	 * Gets the User Adapter Type Parameter from the user.
+	 *
+	 * @param   string|integer|JUser|array  $user  User to inspect.
+	 *
+	 * @return  string  The type for the user if specified.
+	 *
+	 * @since   2.0
+	 */
+	public static function getTypeParam($user = null)
+	{
+		if (is_null($user) || is_numeric($user))
+		{
+			// The input variable indicates we must load the user object
+			$type = JFactory::getUser($user)->getParam(self::PARAM_AUTH_TYPE);
+		}
+		elseif ($user instanceof JUser)
+		{
+			// Direct access of the object
+			$type = $user->getParam(self::PARAM_AUTH_TYPE);
+		}
+		elseif (is_array($user))
+		{
+			if (isset($user['params']))
+			{
+				// Load the user parameters into a registry object for inspection
+				$reg = new JRegistry;
+				$reg->loadString($user['params']);
+
+				$type = $reg->get(self::PARAM_AUTH_TYPE);
+			}
+			else
+			{
+				return null;
+			}
+		}
+		else
+		{
+			// Assume string
+			$id = JUserHelper::getUserId((string) $user);
+			$type = JFactory::getUser($id)->getParam(self::PARAM_AUTH_TYPE);
+		}
+
+		if (!empty($type))
+		{
+			return $type;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Gets the Domain Parameter from the user.
+	 *
+	 * @param   string|integer|JUser|array  $user  User to inspect.
+	 *
+	 * @return  string  The domain for the user if specified.
+	 *
+	 * @since   2.0
+	 */
+	public static function getDomainParam($user = null)
+	{
+		if (is_null($user) || is_numeric($user))
+		{
+			// The input variable indicates we must load the user object
+			$domain = JFactory::getUser($user)->getParam(self::PARAM_AUTH_DOMAIN);
+		}
+		elseif ($user instanceof JUser)
+		{
+			// Direct access of the object
+			$domain = $user->getParam(self::PARAM_AUTH_DOMAIN);
+		}
+		elseif (is_array($user))
+		{
+			if (isset($user['params']))
+			{
+				// Load the user parameters into a registry object for inspection
+				$reg = new JRegistry;
+				$reg->loadString($user['params']);
+
+				$domain = $reg->get(self::PARAM_AUTH_DOMAIN);
+			}
+			else
+			{
+				return null;
+			}
+		}
+		else
+		{
+			// Assume string
+			$id = JUserHelper::getUserId((string) $user);
+			$domain = JFactory::getUser($id)->getParam(self::PARAM_AUTH_DOMAIN);
+		}
+
+		if (!empty($domain))
+		{
+			return $domain;
+		}
+
+		return null;
 	}
 
 	/**
