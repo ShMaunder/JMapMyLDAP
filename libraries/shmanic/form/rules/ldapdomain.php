155911944bb44ddc2d1aa46391b99a13da668255
Shaun Maunder <shmaunder@yahoo.co.uk>
2013-11-06 23:02:02 +0000

    Added ldap domain validation to com_shldap and mod_shldap_login

---
+++ libraries/shmanic/form/rules/ldapdomain.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * PHP Version 5.3
+ *
+ * @package     Shmanic.Libraries
+ * @subpackage  Form.Rules
+ * @author      Shaun Maunder <shaun@shmanic.com>
+ *
+ * @copyright   Copyright (C) 2011-2012 Shaun Maunder. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Form rule for testing the LDAP domain.
+ *
+ * @package     Shmanic.Libraries
+ * @subpackage  Form.Rules
+ * @since       2.0
+ */
+class SHFormRuleLdapdomain extends JFormRule
+{
+	/**
+	 * The regular expression to use in testing a form field value.
+	 *
+	 * @var    string
+	 * @since  11.1
+	 */
+	protected $regex = '[A-Z0-9_\.-\s]+$';
+
+	/**
+	 * The regular expression modifiers to use when testing a form field value.
+	 *
+	 * @var    string
+	 * @since  11.1
+	 */
+	protected $modifiers = 'i';
+}
