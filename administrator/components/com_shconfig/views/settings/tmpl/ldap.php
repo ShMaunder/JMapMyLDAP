810a1bae43ba72c631396ea4fba0c265a33ba5fc
Shaun Maunder <shmaunder@yahoo.co.uk>
2013-11-10 22:37:04 +0000

    Added extra info to com_shconfig ldap settings

---
+++ administrator/components/com_shconfig/views/settings/tmpl/ldap.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * PHP Version 5.3
+ *
+ * @package     Shmanic.Components
+ * @subpackage  Shldap
+ * @author      Shaun Maunder <shaun@shmanic.com>
+ *
+ * @copyright   Copyright (C) 2011-2013 Shaun Maunder. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.tooltip');
+JHtml::_('behavior.formvalidation');
+?>
+<script type="text/javascript">
+	Joomla.submitbutton = function(task)
+	{
+		if (document.formvalidator.isValid(document.id('settings-form')))
+		{
+			Joomla.submitform(task, document.getElementById('settings-form'));
+		}
+	}
+</script>
+<form action="<?php echo JRoute::_('index.php?option=com_shconfig&task=settings.edit&layout=ldap'); ?>" method="post" name="adminForm" id="settings-form" class="form-validate">
+	<div class="row-fluid">
+	<div class="width-50 fltlft span6 form-horizontal">
+		<fieldset class="adminform">
+			<legend><?php echo JText::_('COM_SHCONFIG_SETTINGS_LDAP_TITLE'); ?></legend>
+			<div class="adminformlist tab-content">
+				<div class="tab-pane active" id="details">
+				<?php foreach ($this->form->getFieldset('ldap') as $field) : ?>
+					<?php if (!$field->hidden) : ?>
+						<div class="control-group"><?php echo $field->label; ?>
+						<?php echo $field->input; ?></div>
+					<?php endif; ?>
+				<?php endforeach; ?>
+				</div>
+			</div>
+		</fieldset>
+	</div>
+
+	<div class="width-50 fltrt span6 form-horizontal">
+		<fieldset class="adminform">
+			<legend><?php echo JText::_('COM_SHCONFIG_SETTINGS_LDAP_INFO_TITLE'); ?></legend>
+			<div class="adminformlist tab-content">
+				<div class="tab-pane active" id="details">
+					<div id="ldap-info"><?php echo JText::_('COM_SHCONFIG_SETTINGS_LDAP_INFO_TEXT'); ?></div>
+				</div>
+			</div>
+		</fieldset>
+	</div>
+
+	<div class="clearfix"> </div>
+
+	<div>
+		<input type="hidden" name="task" value="" />
+		<?php echo JHtml::_('form.token'); ?>
+	</div>
+	</div>
+</form>
