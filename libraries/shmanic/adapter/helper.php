54ab663e859fc4e713e0e5a525c7f1406f944c76
Shaun Maunder <shmaunder@yahoo.co.uk>
2014-02-23 13:12:21 +0000

    Adapter helper commitChanges is now generic

--- libraries/shmanic/adapter/helper.php
+++ libraries/shmanic/adapter/helper.php
@@ -1,22 +1,42 @@
 <?php
+/**
+ * PHP Version 5.3
+ *
+ * @package     Shmanic.Libraries
+ * @subpackage  Adapter
+ * @author      Shaun Maunder <shaun@shmanic.com>
+ *
+ * @copyright   Copyright (C) 2011-2013 Shaun Maunder. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Helper class for SHAdapter.
+ *
+ * @package     Shmanic.Libraries
+ * @subpackage  Adapter
+ * @since       2.1
+ */
 abstract class SHAdapterHelper
 {
 	/**
-	 * Commits the changes to the LDAP user adapter and parses the result.
+	 * Commits the changes to the adapter and parses the result.
 	 * If any errors occurred then optionally log them and throw an exception.
 	 *
-	 * @param   SHUserAdaptersLdap  $adapter  LDAP user adapter.
-	 * @param   boolean             $log      Log any errors directly to SHLog.
-	 * @param   boolean             $throw    Throws an exception on error OR return array on error.
+	 * @param   SHAdapter  $adapter  Adapter.
+	 * @param   boolean    $log      Log any errors directly to SHLog.
+	 * @param   boolean    $throw    Throws an exception on error OR return array on error.
 	 *
 	 * @return  true|array
 	 *
-	 * @exception
+	 * @since   2.1
 	 */
 	public static function commitChanges($adapter, $log = false, $throw = true)
 	{
 		$results = $adapter->commitChanges();
+		$adapterName = $adapter->getName();
 
 		if ($log)
 		{
@@ -25,12 +45,12 @@ abstract class SHAdapterHelper
 			{
 				if ($commit['status'] === JLog::INFO)
 				{
-					SHLog::add($commit['info'], 10634, JLog::INFO, 'ldap');
+					SHLog::add($commit['info'], 10634, JLog::INFO, $adapterName);
 				}
 				else
 				{
-					SHLog::add($commit['info'], 10636, JLog::ERROR, 'ldap');
-					SHLog::add($commit['exception'], 10637, JLog::ERROR, 'ldap');
+					SHLog::add($commit['info'], 10636, JLog::ERROR, $adapterName);
+					SHLog::add($commit['exception'], 10637, JLog::ERROR, $adapterName);
 				}
 			}
 		}
@@ -40,7 +60,7 @@ abstract class SHAdapterHelper
 		{
 			if ($throw)
 			{
-				throw new RuntimeException(JText::_('LIB_SHLDAPHELPER_ERR_10638'), 10638);
+				throw new RuntimeException(JText::_('LIB_SHADAPTERHELPER_ERR_10638'), 10638);
 			}
 			else
 			{
