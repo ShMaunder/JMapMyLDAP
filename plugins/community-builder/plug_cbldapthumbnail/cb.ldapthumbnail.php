1a2dae6e1b308f868c5a2ada6075eb74b54e09a7
Kiran Cheema <kiran.cheema@nhs.net>
2015-03-20 19:22:22 +0000

    Update cb.ldapthumbnail.php
    Updated method - returns as image feild and handles blank images with avatar default inamge

--- plugins/community-builder/plug_cbldapthumbnail/cb.ldapthumbnail.php
+++ plugins/community-builder/plug_cbldapthumbnail/cb.ldapthumbnail.php
@@ -1,56 +1,74 @@
 <?php
-/**
-* Joomla Community Builder CB ldap thumbnail Type Plugin: plug_cbldapthumbnail
-* @version $Id$
-* @package plug_cbldapthumbnail
-* @subpackage cb.ldapthumbnail.php
-* @author Kiran Cheema
-* @copyright (C) 2014 
-* @license Limited http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU/GPL version 2
-* @final 1.2
-*/
+use CB\Database\Table\UserTable;
+use CB\Database\Table\FieldTable;
 
-/** ensure this file is being included by a parent file */
 if ( ! ( defined( '_VALID_CB' ) || defined( '_JEXEC' ) || defined( '_VALID_MOS' ) ) ) { die( 'Direct Access to this location is not allowed.' ); }
 
 global $_PLUGINS;
-$_PLUGINS->loadPluginGroup( 'user', array( (int) 1 ) );
-$_PLUGINS->registerUserFieldTypes( array( 'ldapthumbnail' => 'CBfield_ldapthumbnail' ) );
-$_PLUGINS->registerUserFieldParams();
 
-class CBfield_ldapthumbnail extends cbFieldHandler {
-		function getField( &$field, &$user, $output, $reason, $list_compare_types ) {
-		global $_CB_framework, $ueConfig, $_CB_database;
-		
-		$oReturn = null;
-		//grab user profile id 
-		$thisuser = $user->id;
-	
-	// grab ldap.thumbnailPhoto from user_profile table 
-	$db = JFactory::getDBO();
-	$query = $db->getQuery(true);
-	$query->select($db->quoteName('profile_value'))	
-		->from($db->quoteName('#__user_profiles'))
-		->where($query->quoteName('profile_key') . ' = ' . $query->quote('ldap.thumbnailPhoto'))
-		->where($query->quoteName('user_id') . ' = ' . $query->quote((int) $thisuser));
-	$db->setQuery($query);
-	$result=$db->loadResult();
-	
-	//check the result of the Query from the DB
-	if($result!==null){
-		//if there is something in the result stream back the image
-			$image=	'<img src="data:image/jpeg;base64,'.$result.'"/>';
-		}else{
-		//if no image from Ldap then check CB avatar field
-			$cbUser =& CBuser::getInstance( $thisuser );
-			if ( $cbUser !== null ) {
-				//$thumbnailAvatarHtmlWithLink 	= $cbUser->getField( 'avatar', null, 'html', 'none', 'list' );
-				$bigAvatarHtmlWithLink = $cbUser->getField( 'avatar' );
-				$image = $bigAvatarHtmlWithLink;			
-			}
-}
-		$oReturn=$image;	
-		return $oReturn;
+$_PLUGINS->loadPluginGroup( 'user' );
+
+$_PLUGINS->registerUserFieldParams();
+$_PLUGINS->registerUserFieldTypes( array( 'ldapthumbnail' => 'CBfield_ldapthumbnail' ));
+
+class CBfield_ldapthumbnail extends CBfield_image
+{
+
+	/**
+	 * Returns a field in specified format
+	 *
+	 * @param  FieldTable  $field
+	 * @param  UserTable   $user
+	 * @param  string      $output  'html', 'xml', 'json', 'php', 'csvheader', 'csv', 'rss', 'fieldslist', 'htmledit'
+	 * @param  string      $reason  'profile' for user profile view, 'edit' for profile edit, 'register' for registration, 'list' for user-lists
+	 * @param  int         $list_compare_types   IF reason == 'search' : 0 : simple 'is' search, 1 : advanced search with modes, 2 : simple 'any' search
+	 * @return mixed
+	 */
+	public function getField( &$field, &$user, $output, $reason, $list_compare_types ) {
+		if ( $output == 'htmledit' ) {
+			// There's no edit or search display for this calculated field so suppress the core image field output:
+			return null;
+		}
+
+		return parent::getField( $field, $user, $output, $reason, $list_compare_types );
 	}
-}//end of ldap Thumbnail field
-?>
+
+	/**
+	 * returns full or thumbnail path of image
+	 *
+	 * @param  FieldTable   $field
+	 * @param  UserTable    $user
+	 * @param  boolean      $thumbnail
+	 * @param  int          $showAvatar
+	 * @return null|string
+	 */
+	public function _avatarLivePath( &$field, &$user, $thumbnail = true, $showAvatar = 2 ) {
+		global $_CB_database;
+
+		$return				=	null;
+
+		if ( $user && $user->id ) {
+			$query			=	'SELECT ' . $_CB_database->NameQuote( 'profile_value' )
+							.	"\n FROM " . $_CB_database->NameQuote( '#__user_profiles' )
+							.	"\n WHERE " . $_CB_database->NameQuote( 'profile_key' ) . " = " . $_CB_database->Quote( 'ldap.thumbnailPhoto' )
+							.	"\n AND " . $_CB_database->NameQuote( 'user_id' ) . " = " . (int) $user->get( 'id' );
+			$_CB_database->setQuery( $query, 0, 1 );
+			$return			=	$_CB_database->loadResult();
+
+			if ( $return ) {
+				$return		=	'data:image/jpeg;base64,' . $return;
+			}
+		}
+
+		if ( ! $return ) {
+			// If there is no image lets fallback to the avatar path:
+			$return			=	CBuser::getInstance( (int) $user->get( 'id' ) )->getField( 'avatar', null, 'php', 'none', ( $thumbnail ? 'list' : 'profile' ) );
+
+			if ( is_array( $return ) ) {
+				$return		=	$return['avatar'];
+			}
+		}
+
+		return $return;
+	}
+}
